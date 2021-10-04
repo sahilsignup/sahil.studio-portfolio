784b241c6f3a61bbfc87b1e2513e3e28e5d3647f
Sahil <sahilsignup@gmail.com>
2021-10-03 21:02:55 -0400

    Add files via upload

---
+++ home-page.js
@@ -0,0 +1,27 @@
+
+let constrain = 50;
+let mouseOverContainer = document.getElementById("content");
+let ex1Layer = document.getElementById("home-introduction");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(2500px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
