b241d24b63a10b50635b0695e4084b73326d7adb
Sahil <sahilsignup@gmail.com>
2022-08-21 17:18:10 -0400

    Add files via upload

--- JS/resources.js
+++ JS/resources.js
@@ -1,14 +1,14 @@
 $("#portfolio-flters li").click ( function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-
-    var selectedFilter = $(this).data("filter");
-    $("#portfolio-wrapper").fadeTo(100, 0);
-
-    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
-
-    setTimeout(function() {
-      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
-      $("#portfolio-wrapper").fadeTo(400, 1);
-    }, 400);
-  });
+  $("#portfolio-flters li").removeClass('filter-active');
+  $(this).addClass('filter-active');
+  
+  var selectedFilter = $(this).data("filter");
+  $("#portfolio-wrapper").fadeTo(100, 0);
+  
+  $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+  
+  setTimeout(function() {
+    $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+    $("#portfolio-wrapper").fadeTo(300, 1);
+  }, 300);
+});
